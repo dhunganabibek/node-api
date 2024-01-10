d8f0ca4139395a83f1e4d29298ccd99fa8942164
Bibek Dhungana <bibek.dhungana@precisionplanting.com>
2024-01-09 20:31:59 -0600

    CRUD completed

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('Hello');
+import mysql from 'mysql2/promise';
+var db = mysql.createPool({
+    user: 'root',
+    database: 'node-api',
+    host: 'localhost',
+    password: process.env.password,
+});
+//SELECT statement
+var _a = await db.query('SELECT * FROM student'), result = _a[0], structure = _a[1];
+console.log(result);
+//UPDATE statement
+// const r = await db.query('UPDATE student SET age=26 WHERE id=1');
+// console.log(r);
+// DELETE
+// const result = await db.query('DELETE FROM student WHERE id = ?', [1]);
+// console.log(result);
+// // INSERT
+// const result = await db.query(
+//   'INSERT INTO student (name, age,grade,address) VALUES ("Santona", 25, "A", "Lubbock,Texas")'
+// );
+// console.log(result);
+function deleteFromDatabase(table, primarykey) {
+    db.query('DELETE FROM ?? WHERE id = ?', [table, primarykey]);
+}
+deleteFromDatabase('student', 1);
+deleteFromDatabase('subject', 1);
--- index.ts
+++ index.ts
@@ -1,1 +1,33 @@
-console.log('Hello');
+import mysql from 'mysql2/promise';
+
+const db = mysql.createPool({
+  user: 'root',
+  database: 'node-api',
+  host: 'localhost',
+  password: process.env.password,
+});
+
+//SELECT statement
+const [result, structure] = await db.query('SELECT * FROM student');
+console.log(result);
+
+//UPDATE statement
+// const r = await db.query('UPDATE student SET age=26 WHERE id=1');
+// console.log(r);
+
+// DELETE
+// const result = await db.query('DELETE FROM student WHERE id = ?', [1]);
+// console.log(result);
+
+// // INSERT
+// const result = await db.query(
+//   'INSERT INTO student (name, age,grade,address) VALUES ("Santona", 25, "A", "Lubbock,Texas")'
+// );
+// console.log(result);
+
+function deleteFromDatabase(table: string, primarykey: number) {
+  db.query('DELETE FROM ?? WHERE id = ?', [table, primarykey]);
+}
+
+deleteFromDatabase('student', 1);
+deleteFromDatabase('subject', 1);
